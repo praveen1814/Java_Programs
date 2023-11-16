70961b3a1e2d6b0f2778ffef636c6857ab250ce5
Praveen <praveenkumarpr1814@gmail.com>
2023-11-16 21:40:50 +0530

    First Commit

---
+++ EvenorOdd1to100.java
@@ -0,0 +1,24 @@
+package java_programs;
+
+public class EvenorOdd1to100 {
+
+	public static void main(String[] args) {
+		/*int num=1;  //using if condition
+		while(num<=100)
+		{
+			if(num%2==0)
+			{
+				System.out.println(num);
+			}
+			num++;
+		}
+*/
+		int num=2;  //another method without if condition
+		while(num<=100)
+		{
+			System.out.println(num);
+			num+=2;
+		}
+	}
+
+}
