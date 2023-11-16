e92a0540b5da63574ed35338ed5ca15dc8ad4924
Praveen <praveenkumarpr1814@gmail.com>
2023-11-16 21:39:09 +0530

    First Commit

---
+++ EvenOdd.java
@@ -0,0 +1,18 @@
+package java_programs;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		int num=45;
+		if(num%2==0)
+		{
+			System.out.println("Even");
+		}
+		else
+		{
+			System.out.println("Odd");
+		}
+
+	}
+
+}
