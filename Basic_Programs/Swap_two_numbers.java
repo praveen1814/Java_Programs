ae7e6e22f6461e4c1da091ee675029cc2a6fee70
Praveen Kumar <109404771+praveen1814@users.noreply.github.com>
2024-08-20 21:36:09 +0530

    basic programs

---
+++ Basic_Programs/Swap_two_numbers.java
@@ -0,0 +1,18 @@
+package Basic_Programs;
+
+public class Swap_two_numbers {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		
+		a=a+b; //10+20=30
+		b=a-b; //30-20=10
+		a=a-b; //30-10=20
+		
+		System.out.println("before swaping a=10 , b=20 ");
+		System.out.println("after sqpaing  a = "+a +" b = "+b);
+
+	}
+
+}
